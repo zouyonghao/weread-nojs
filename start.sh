1075b7e1841a33a2599bf5eba627dc031ef4d93c
Yonghao Zou <yonghaoz1994@gmail.com>
2024-09-29 21:22:00 +0200

    Add no sandbox

---
+++ start.sh
@@ -0,0 +1,12 @@
+service cloudflared start
+service dbus start
+
+Xvfb :99 -screen 0 1920x1080x24 > /dev/null 2>&1 &
+export DISPLAY=:99
+
+cd /root/weread-nojs
+
+pnpm build > /dev/null 2>&1
+pnpm start > /dev/null 2>&1 &
+
+bash
